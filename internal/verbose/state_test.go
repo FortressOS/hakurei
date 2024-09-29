df29068d1683e044fddf6dd0d0ec24018d66cdc6
Ophestra Umiker <cat@ophivana.moe>
2024-09-29 16:36:59 +0900

    verbose: test verbose behaviour
    Signed-off-by: Ophestra Umiker <cat@ophivana.moe>

---
+++ internal/verbose/state_test.go
@@ -0,0 +1,20 @@
+package verbose_test
+
+import (
+	"testing"
+
+	"git.ophivana.moe/cat/fortify/internal/verbose"
+)
+
+func TestGetSet(t *testing.T) {
+	verbose.Set(false)
+	if verbose.Get() {
+		t.Errorf("Get() = true, want false")
+	}
+
+	verbose.Set(true)
+	if !verbose.Get() {
+		t.Errorf("Get() = false, want true")
+	}
+
+}
