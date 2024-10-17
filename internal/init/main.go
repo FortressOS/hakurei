FortressOS/hakurei

commit 5401882ed0f4e6ca28d1312bc29b90bb4d64a04f

init: post initial process death exit timeout
Wait for 5 seconds before printing a message and exiting after picking up the initial process's wait status. This also kills any lingering processes.This behaviour is helpful for applications launched without a terminal attached.

Signed-off-by: Ophestra Umiker <cat@ophivana.moe>

internal/init/main.go
@@ -11,8 +11,14 @@ import (
 	"path"
 	"strconv"
 	"syscall"
+	"time"
 
 	"git.ophivana.moe/cat/fortify/internal/verbose"
+)
+
+const (
+	// time to wait for linger processes after death initial process
+	residualProcessTimeout = 5 * time.Second
 )
 
 // everything beyond this point runs within pid namespace
@@ -121,6 +127,8 @@ func doInit(fd uintptr) {
 		close(done)
 	}()
 
+	timeout := make(chan struct{})
+
 	r := 2
 	for {
 		select {
@@ -138,7 +146,14 @@ func doInit(fd uintptr) {
 					r = 255
 				}
 			}
+			go func() {
+				time.Sleep(residualProcessTimeout)
+				close(timeout)
+			}()
 		case <-done:
+			os.Exit(r)
+		case <-timeout:
+			fmt.Println("fortify-init: timeout exceeded waiting for lingering processes")
 			os.Exit(r)
 		}
 	}

commit 919e5b5cd5deb16087f4a85e2801e73643d7dce4

init: start timeout only if reaped PID is the initial process
Fix a very obvious bug introduced in 5401882ed0.

Signed-off-by: Ophestra Umiker <cat@ophivana.moe>

internal/init/main.go
@@ -145,11 +145,12 @@ func doInit(fd uintptr) {
 				default:
 					r = 255
 				}
+
+				go func() {
+					time.Sleep(residualProcessTimeout)
+					close(timeout)
+				}()
 			}
-			go func() {
-				time.Sleep(residualProcessTimeout)
-				close(timeout)
-			}()
 		case <-done:
 			os.Exit(r)
 		case <-timeout: