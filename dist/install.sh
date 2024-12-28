ab48706ebeec03cac0a1fcbab51e6793cf96ecd3
Ophestra <cat@gensokyo.uk>
2024-12-29 01:04:53 +0900

    dist: install fpkg to /usr/bin
    This is a high level user-facing tool.
    
    Signed-off-by: Ophestra <cat@gensokyo.uk>

--- dist/install.sh
+++ dist/install.sh
@@ -2,9 +2,10 @@
 cd "$(dirname -- "$0")" || exit 1
 
 install -vDm0755 "bin/fortify" "${FORTIFY_INSTALL_PREFIX}/usr/bin/fortify"
+install -vDm0755 "bin/fpkg" "${FORTIFY_INSTALL_PREFIX}/usr/bin/fpkg"
+
 install -vDm0755 "bin/fshim" "${FORTIFY_INSTALL_PREFIX}/usr/libexec/fortify/fshim"
 install -vDm0755 "bin/finit" "${FORTIFY_INSTALL_PREFIX}/usr/libexec/fortify/finit"
-install -vDm0755 "bin/fpkg" "${FORTIFY_INSTALL_PREFIX}/usr/libexec/fortify/fpkg"
 install -vDm0755 "bin/fuserdb" "${FORTIFY_INSTALL_PREFIX}/usr/libexec/fortify/fuserdb"
 
 install -vDm6511 "bin/fsu" "${FORTIFY_INSTALL_PREFIX}/usr/bin/fsu"
