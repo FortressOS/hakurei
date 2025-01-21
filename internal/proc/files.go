82029948e6d8d047edc02ccff354e16c419e5742
Ophestra <cat@gensokyo.uk>
2025-01-21 12:51:39 +0900

    proc: append to ExtraFiles slice pointer
    This is useful for initialising extra files before command.
    
    Signed-off-by: Ophestra <cat@gensokyo.uk>

--- internal/proc/files.go
+++ internal/proc/files.go
@@ -6,8 +6,12 @@ import (
 )
 
 func ExtraFile(cmd *exec.Cmd, f *os.File) (fd uintptr) {
+	return ExtraFileSlice(&cmd.ExtraFiles, f)
+}
+
+func ExtraFileSlice(extraFiles *[]*os.File, f *os.File) (fd uintptr) {
 	// ExtraFiles: If non-nil, entry i becomes file descriptor 3+i.
-	fd = uintptr(3 + len(cmd.ExtraFiles))
-	cmd.ExtraFiles = append(cmd.ExtraFiles, f)
+	fd = uintptr(3 + len(*extraFiles))
+	*extraFiles = append(*extraFiles, f)
 	return
 }
