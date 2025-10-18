f5845e312e0b74b54c3884bb5b4df9d285e26f9a
Ophestra <cat@gensokyo.uk>
2025-10-18 23:46:10 +0900

    internal/app/sptmpdir: check behaviour
    Another simple one. This will change when shared tmpdir and xdg runtime dir becomes optional.
    
    Signed-off-by: Ophestra <cat@gensokyo.uk>

---
+++ internal/app/sptmpdir_test.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"testing"
+
+	"hakurei.app/container"
+	"hakurei.app/container/bits"
+	"hakurei.app/container/fhs"
+	"hakurei.app/container/stub"
+	"hakurei.app/hst"
+	"hakurei.app/system"
+	"hakurei.app/system/acl"
+)
+
+func TestSpTmpdirOp(t *testing.T) {
+	t.Parallel()
+
+	checkOpBehaviour(t, []opBehaviourTestCase{
+		{"success", func(bool, bool) outcomeOp {
+			return spTmpdirOp{}
+		}, hst.Template, nil, []stub.Call{
+			// this op configures the system state and does not make calls during toSystem
+		}, newI().
+			Ensure(m("/proc/nonexistent/tmp/hakurei.0/tmpdir"), 0700).
+			UpdatePermType(system.User, m("/proc/nonexistent/tmp/hakurei.0/tmpdir"), acl.Execute).
+			Ensure(m("/proc/nonexistent/tmp/hakurei.0/tmpdir/9"), 01700).
+			UpdatePermType(system.User, m("/proc/nonexistent/tmp/hakurei.0/tmpdir/9"), acl.Read, acl.Write, acl.Execute), nil, nil, insertsOps(nil), []stub.Call{
+			// this op configures the container state and does not make calls during toContainer
+		}, &container.Params{
+			Ops: new(container.Ops).
+				Bind(m("/proc/nonexistent/tmp/hakurei.0/tmpdir/9"), fhs.AbsTmp, bits.BindWritable),
+		}, nil, nil},
+	})
+}
