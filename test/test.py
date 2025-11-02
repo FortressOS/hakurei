0edcb7c1d39bf92cfae7ebcaab278620ca35ef11
Ophestra <cat@gensokyo.uk>
2025-11-02 17:00:59 +0900

    test: print share directory
    This is more useful now that state is tracked here.
    
    Signed-off-by: Ophestra <cat@gensokyo.uk>

--- test/test.py
+++ test/test.py
@@ -271,7 +271,11 @@ machine.wait_until_fails("pgrep foot", timeout=5)
 swaymsg("exit", succeed=False)
 machine.wait_for_file("/tmp/sway-exit-ok")
 
-# Print hakurei runDir contents:
+# Print hakurei share and rundir contents:
+print(machine.succeed("find /tmp/hakurei.0 "
+    + "-path '/tmp/hakurei.0/runtime/*/*' -prune -o "
+    + "-path '/tmp/hakurei.0/tmpdir/*/*' -prune -o "
+    + "-print"))
 print(machine.succeed("find /run/user/1000/hakurei"))
 
 # Verify go test status:
